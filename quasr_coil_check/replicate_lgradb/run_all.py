b5ac007ab74219a6b8e046cd8febeb2f788a7672
Philipp Jurasic <philipp.jurasic@ipp.mpg.de>
2024-08-25 14:27:02 +0200

    prepare script for lgradB analysis

---
+++ quasr_coil_check/replicate_lgradb/run_all.py
@@ -0,0 +1,12 @@
+import find_single_l
+import os
+import simsopt
+
+for top, dirs, files in os.walk("db"):
+    for file in files:
+        if os.path.splitext(file)[1] != ".json":
+            continue
+        path = os.path.join(top, file)
+        surfs, coils = simsopt.load(path)
+        res = find_single_l.find_regcoil_distance(surfs[-1])
+        print(res)
