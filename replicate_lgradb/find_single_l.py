7545b8f7089718aa6b7c98ba774e273660f651de
Philipp Jurasic <philipp.jurasic@ipp.mpg.de>
2024-08-29 14:47:24 +0200

    lgradb analysis

--- replicate_lgradb/find_single_l.py
+++ replicate_lgradb/find_single_l.py
@@ -16,6 +16,15 @@ REGCOIL_IN_TMP_PATH = "replicate_lgradb/tmp/regcoil_in.python_generated"
 REGCOIL_OUT_TMP_PATH = "replicate_lgradb/tmp/regcoil_out.python_generated.nc"
 PLASMA_PATH = "replicate_lgradb/tmp/wout_surfaces_python_generated.nc"
 
+class Memoization:
+    def __init__(self, f):
+        self.f = f
+        self.dict = {}
+    def __call__(self, *args):
+        if not args in self.dict:
+            self.dict[args] = self.f(*args)
+        print("kinfty_at",*args,"=",self.dict[args])
+        return self.dict[args]
 
 def find_regcoil_distance(lcfs):
     """Find the distance the coil surface must be separated from the plasma, to fulfill
@@ -23,10 +32,11 @@ def find_regcoil_distance(lcfs):
     The optimization is bounded, the surface offset is ]0, 0.5["""
     target_k = 17.16e6
 
+    @Memoization
     def kinfty_at_regcoil_distance(l: float):
         # Run Regcoil at offset l
         log = run_regcoil_fixed_dist(lcfs, l).decode("utf-8")
-        print(log)
+        # print(log)
         if "it is too low." in log:
             return np.inf
         if "it is too high." in log:
@@ -34,14 +44,31 @@ def find_regcoil_distance(lcfs):
         # Read result, extract K_infty
         with netcdf_file(REGCOIL_OUT_TMP_PATH, "r", mmap=False) as f:
             k_infty = f.variables["max_K"][()]
-            return k_infty
+            return k_infty[-1]
+
+    search_interval = np.array([1e-2,0.35])*5
+    search_initial_points = np.linspace(search_interval[0],search_interval[1], 5)
+    kinftys = np.array([kinfty_at_regcoil_distance(float(l)) for l in search_initial_points])
+
+    # Comma for tuple unpacking
+    lower_bound_idx, = np.where(np.isfinite(kinftys) & (kinftys <= target_k))
+    if len(lower_bound_idx)==0:
+        return np.inf
+    # Largest lower bound (expects kinftys order to be increasing)
+    search_interval[0] = search_initial_points[lower_bound_idx[-1]]
+    
+    # Comma for tuple unpacking
+    upper_bound_idx, = np.where(np.isfinite(kinftys) & (kinftys >= target_k))
+    if len(upper_bound_idx)==0:
+        return np.inf
+    # Smallest upper bound (expects kinftys order to be increasing)
+    search_interval[1] = search_initial_points[upper_bound_idx[0]]
+    print("scipy.optimize in interval",search_interval)
 
     opt_result = scipy.optimize.root_scalar(
-        lambda l: kinfty_at_regcoil_distance(float(l)) - target_k, bracket=[1e-3,0.5],# tol=target_k * 1e-6, method="brentq"
+        lambda l: kinfty_at_regcoil_distance(float(l) - target_k), bracket=search_interval.tolist(),# tol=target_k * 1e-6, method="brentq"
     )
     l_result = opt_result.root
-    assert l_result > 0
-    assert l_result < 0.5
     return l_result
 
 
@@ -49,7 +76,7 @@ def run_regcoil_fixed_dist(plasma_surface: simsopt.geo.Surface, distance: float)
     bdistrib_io.write_netcdf(PLASMA_PATH, plasma_surface.to_RZFourier())
     cwd = os.path.dirname(REGCOIL_IN_TMP_PATH)
 
-    surface_resolution = 128
+    surface_resolution = 96
     input_string = f"""&regcoil_nml
   general_option = 5 ! Check if target is attainable first
   Nlambda = 16
@@ -75,5 +102,4 @@ def run_regcoil_fixed_dist(plasma_surface: simsopt.geo.Surface, distance: float)
 """
     with open(REGCOIL_IN_TMP_PATH, "w") as f:
         f.write(input_string)
-    print(f"-----------{distance}----------")
     return subprocess.check_output(["regcoil", os.path.basename(REGCOIL_IN_TMP_PATH)], cwd=cwd)
--- replicate_lgradb/run_all.py
+++ replicate_lgradb/run_all.py
@@ -6,7 +6,8 @@ for top, dirs, files in os.walk("replicate_lgradb/db"):
     for file in files:
         if os.path.splitext(file)[1] != ".json":
             continue
+        print(file)
         path = os.path.join(top, file)
         surfs, coils = simsopt.load(path)
         res = find_single_l.find_regcoil_distance(surfs[-1])
-        print(res)
+        print("result=",res)
