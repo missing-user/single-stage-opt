b5ae37bc15032fe048471c93f478fef66a22f633
Philipp Jurasic <philipp.jurasic@ipp.mpg.de>
2024-10-14 13:02:31 +0200

    lackner metric plot subplot grid (broken)

--- hybrid_tokamak/plot_virtual_casing_current.py
+++ hybrid_tokamak/plot_virtual_casing_current.py
@@ -7,21 +7,37 @@ import matplotlib.pyplot as plt
 eq_fam = desc.io.load("input.NAS.nv.n4.SS.iota43.Fake-ITER.01_output.h5")
 eq = eq_fam[-1]
 eq_fam2 = desc.io.load(
-    "input.NAS.nv.n4.SS.iota43.Fake-ITER.unperturbed.01_output.h5")
+    "input.NAS.nv.n4.SS.iota43.Fake-ITER_unperturbed.01_desc_output.h5"
+)
 eq2 = eq_fam2[-1]
 
 desc.plotting.plot_comparison([eq, eq2])
-desc.plotting.plot_2d(eq2, "K_vc")
 
-# desc.plotting.plot_2d(eq, "J")
-desc.plotting.plot_2d(eq, "K_vc")
-plt.figure()
 
-grid = desc.grid.LinearGrid(theta=100, zeta=100, NFP=eq.NFP)
-computed = eq.compute(["n_rho", "B"], grid)
-K = np.cross(computed["n_rho"], computed["B"], axis=-1)
-K_abs = np.linalg.norm(K, axis=-1)
-K_outer = grid.meshgrid_reshape(K_abs, "rtz")[0]
-plt.contourf(K_outer)
+def manual_Kvc_calculation(eqilibrium, grid):
+    MU0 = 4 * np.pi * 1e-6
+    computed = eqilibrium.compute(["n_rho", "B"], grid)
+    K = np.cross(computed["n_rho"], computed["B"], axis=-1) / MU0
+    K_abs = np.linalg.norm(K, axis=-1)
+    return K_abs
+
+
+def get_Kvc_lcfs(eqilibrium, grid):
+    computed = eqilibrium.compute(["K_vc"], grid)
+    return grid.meshgrid_reshape(computed["K_vc"], "rtz")[0]
+
+
+lcfs_grid = desc.grid.LinearGrid(theta=100, zeta=100, NFP=eq.NFP)
+Kvc = get_Kvc_lcfs(eq, lcfs_grid)
+Kvc2 = get_Kvc_lcfs(eq2, lcfs_grid)
+norm_of_diff = np.linalg.norm(Kvc - Kvc2, axis=-1)
+diff_of_norm = np.linalg.norm(Kvc, axis=-1) - np.linalg.norm(Kvc2, axis=-1)
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+desc.plotting.plot_2d(eq, "K_vc", ax=axs[0, 0])
+desc.plotting.plot_2d(eq2, "K_vc", ax=axs[0, 1])
+axs[1, 0].contourf(norm_of_diff, cmap="jet", levels=100)
+axs[1, 0].set_title("norm of diff K_vc")
+axs[1, 1].contourf(diff_of_norm, cmap="jet", levels=100)
+axs[1, 1].set_title("diff of norm K_vc")
 plt.colorbar()
 plt.show()
