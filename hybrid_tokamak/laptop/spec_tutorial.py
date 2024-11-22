a70eb503ed22f45f305a8a8feaa1cdbb1338992f
Philipp Jurasic <philipp.jurasic@tum.de>
2024-11-22 14:14:16 +0100

    working vacuum well target

--- hybrid_tokamak/laptop/spec_tutorial.py
+++ hybrid_tokamak/laptop/spec_tutorial.py
@@ -1,22 +1,24 @@
+import simsopt
 from simsopt import mhd
-from simsopt.objectives import LeastSquaresProblem
+from simsopt import configs #configs.zoo.get()
+
+from simsopt import objectives
 from simsopt.solve import least_squares_serial_solve, least_squares_mpi_solve
 from simsopt import geo
 from simsopt.util import MpiPartition
 import matplotlib.pyplot as plt
 import numpy as np
 
-# import logging
-# logging.basicConfig()
-# logging.getLogger().setLevel(logging.DEBUG)
+import logging
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
 
-mpi = MpiPartition(3)
-
+mpi = MpiPartition(4)
 equil = mhd.Spec(
     "hybrid_tokamak/laptop/nfp2_QA_iota0.4.sp",
-    # "../../SPEC/InputFiles/Verification/FreeBoundVMEC/Nv=002.L=8.M=12.n.sp",
     mpi,
     verbose=False,
+    keep_all_files=True
 )
 # equil.mpol = 7
 # equil.ntor = 7
@@ -30,9 +32,9 @@ msurf.change_resolution(msurf.mpol, 0)
 msurf.scale(1.5)
 # equil.boundary = surf
 surf = equil.boundary
+mmax = 3
+nmax = 3
 surf.fix_all()
-mmax = 1
-nmax = 1
 surf.fixed_range(0, mmax, -nmax, nmax, False)
 surf.fix("rc(0,0)")  # Major radius
 print(equil.dof_names)
@@ -83,57 +85,61 @@ fracs = sorted(
     find_simples_fractions(Fraction(-0.38), Fraction(-0.5), 1),
     key=lambda x: x.denominator,
 )
-print(fracs)
-# Construct the greens residues for the 4 most likely islands. Residue uses Fortran indexing for vol
-island_residues = [
-    (mhd.Residue(equil, frac.numerator, frac.denominator, vol=1, rtol=1e-6).J, 0, 1)
-    for frac in fracs[:1]
-]
-
-# desired_volume = 3.33
-# term2 = (equil.volume, desired_volume, 1.0)
-
 qs = mhd.QuasisymmetryRatioResidualSpec(
     equil, surfaces=np.linspace(0.1, 1, 6), helicity_m=1, helicity_n=0
 )
 
 # vac_well = mhd.VacuumWell(equil, surfaces=np.linspace(0, 1, 11))
-prob = LeastSquaresProblem.from_tuples(
-    [
-        (qs.residuals, 0, 5),
-        (surf.aspect_ratio, 7, 0.1),
-        # (vac_well, 0, 1),
-        # *island_residues
-    ]
-)
-
-
+# prob = objectives.LeastSquaresProblem.from_tuples(
+#     [
+#         (qs.residuals, 0, 5),
+#         (surf.aspect_ratio, 7, 0.1),
+#         # (vac_well, 0, 1),
+#     ]
+# ) + objectives.LeastSquaresProblem.from_tuples(
+#     # Construct the greens residues for the 1 most likely islands. Residue uses Fortran indexing for vol
+#     (mhd.Residue(equil, frac.numerator, frac.denominator, vol=1, rtol=1e-6).J, 0, 1)
+#         for frac in fracs[:1]
+# )
 
 # These don't have __self__ and throw an error
 def iota_average():
     return np.mean(equil.results.transform.fiota[1, :])
-def iota_edge():
+
+def iota_edge(equil):
+    equil.run() 
     return equil.results.transform.fiota[1, -1]
+iota_edge_target = simsopt.make_optimizable(iota_edge, equil) 
 
 
 # iota = p / q
 p = -2
 q = 5
-residue1 = mhd.Residue(equil, p, q)
-residue2 = mhd.Residue(equil, p, q, theta=np.pi)
-prob = LeastSquaresProblem.from_tuples(
+residue1 = mhd.Residue(equil, p, q, s_guess=0.65)
+residue2 = mhd.Residue(equil, p, q, s_guess=0.65, theta=np.pi)
+prob = objectives.LeastSquaresProblem.from_tuples(
     [
+        (qs.residuals, 0, 5),
+        (equil.vacuum_well, 1e-2, 1),
         (surf.aspect_ratio, 6, 1),
         (equil.iota, 0.39, 1),
-        # (iota_edge, 0.42, 1),
-        (qs.residuals, 0, 2),
-        (residue1.J, 0, 2),
-        (residue2.J, 0, 2),
+        (iota_edge_target.J, 0.41, 1),
+        (residue1.J, 0, 1),
+        (residue2.J, 0, 1),
     ]
 )
 
 
-# print(" Initial objective function = ", prob.objective())
+print(prob.dof_names)
+print(" Initial objective function = ", prob.objective())
+print("   Initial Quasisymmetry Ratio = ", qs.total())
+print("   Initial Vacuum Well = ", equil.vacuum_well())
+print("   Initial Surface Aspect Ratio = ", surf.aspect_ratio())
+print("   Initial Equilibrium Iota = ", equil.iota())
+print("   Initial Iota Edge Target = ", iota_edge_target.J())
+print("   Initial Residue 1 = ", residue1.J())
+print("   Initial Residue 2 = ", residue2.J())
+
 if mpi is None:
     least_squares_serial_solve(prob, max_nfev=100)
 else:
@@ -141,36 +147,41 @@ else:
 
 # Run the final iteration with a higher poincare resolution
 
+# equil = mhd.Spec("hybrid_tokamak/laptop/nfp2_QA_iota0.4_000_000082.sp", mpi)
 
+qs = mhd.QuasisymmetryRatioResidualSpec(
+    equil, surfaces=np.linspace(0.1, 1, 6), helicity_m=1, helicity_n=0
+)
 if mpi.proc0_world:
-    equil.inputlist.nptrj = 32
-    equil.inputlist.nppts = 256
+    equil.inputlist.nptrj = 8
+    equil.inputlist.nppts = 128
     equil.recompute_bell()
     equil.run()
-
-    print("At the optimum,")
-    print(" rc(m=1,n=1) = ", surf.get_rc(1, 1))
-    print(" zs(m=1,n=1) = ", surf.get_zs(1, 1))
-    print(" volume, according to SPEC    = ", equil.volume())
-    print(" volume, according to Surface = ", surf.volume())
-    print(
-        " volume_current_profile, according to SPEC  = ", equil.volume_current_profile
-    )
-    print(
-        " interface_current_profile, according to SPEC = ",
-        equil.interface_current_profile,
-    )
-    print(" iota on axis = ", equil.iota())
-    print(" objective function = ", prob.objective())
-    print(" Quasisymmetry Ratio = ", qs[0]())
-
+    
+    print(" Final objective function = ", prob.objective())
+    print("   Final Quasisymmetry Ratio = ", qs.total())
+    print("   Final Vacuum Well = ", equil.vacuum_well())
+    print("   Final Surface Aspect Ratio = ", surf.aspect_ratio())
+    print("   Final Equilibrium Iota = ", equil.iota())
+    print("   Final Iota Edge Target = ", iota_edge_target.J())
+    print("   Final Residue 1 = ", residue1.J())
+    print("   Final Residue 2 = ", residue2.J())
+    
+    targets = "_residual_qs_aspect" 
+    basename = equil.results.filename.removesuffix(".sp.h5") + targets
     equil.results.plot_poincare()
+    plt.savefig( basename+"_poincare.png")
     equil.results.plot_iota(xaxis="s")
+    plt.savefig( equil.results.filename+"_iota.png")
     equil.results.plot_kam_surface()
+    plt.savefig( equil.results.filename+"_kam.png")
     equil.results.plot_modB(
         np.linspace(-0.999, 1, 32), np.linspace(0, 2 * np.pi, 128), np.linspace(0, 0, 2)
     )
-    # equil.results.plot_pressure()
-
-    geo.plot([surf], engine="plotly", close=True)
+    plt.savefig( equil.results.filename+"_modB.png")
     plt.show()
+
+    import coils_for_QA as coilopt
+    coils, J1, J2 =coilopt.optimize(equil.boundary)
+    geo.plot([equil.boundary, *coils], engine="plotly", close=True)
+
