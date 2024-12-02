5f9c27cab93248b314e8f643ba62f2dfff95e35c
Philipp Jurasic <philipp.jurasic@tum.de>
2024-12-02 10:59:18 +0100

    plot boozxform + lgradB

--- hybrid_tokamak/laptop/plot_boozxform.py
+++ hybrid_tokamak/laptop/plot_boozxform.py
@@ -6,10 +6,23 @@ import booz_xform
 import matplotlib.pyplot as plt
 import numpy as np
 
+from simsopt.mhd.vmec_diagnostics import vmec_compute_geometry, vmec_fieldlines
+
 import sys
 
 def boozer_plot(filename):
-    b1 = booz_xform.Booz_xform()
+    vmec = mhd.Vmec(filename)
+    boozer = mhd.Boozer(vmec)
+
+    s = [0.25, 1.0]
+    ntheta = 4
+    nphi = 5
+    theta = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
+    phi = np.linspace(0, 2 * np.pi / vmec.wout.nfp, nphi, endpoint=False)
+    data = vmec_compute_geometry(vmec, s, theta, phi)
+    print("data.L_grad_B", data.L_grad_B)
+
+    b1 = boozer.bx
     b1.read_wout(filename)
     b1.compute_surfs = [5, 64]
     b1.run()
