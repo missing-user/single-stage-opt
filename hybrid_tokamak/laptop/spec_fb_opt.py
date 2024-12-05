e037ab4439e253b01f0142dfbf2c76f6dfbd5f10
Philipp Jurasic <philipp.jurasic@tum.de>
2024-12-05 21:21:37 +0100

    another optimization run, QA

--- hybrid_tokamak/laptop/spec_fb_opt.py
+++ hybrid_tokamak/laptop/spec_fb_opt.py
@@ -1,8 +1,11 @@
+import glob
+import os
 import simsopt
+import datetime
 from simsopt import mhd
 from simsopt import geo
 from simsopt import objectives
-from simsopt.solve import least_squares_mpi_solve
+from simsopt.solve import least_squares_mpi_solve, constrained_mpi_solve
 import matplotlib.pyplot as plt
 import numpy as np
 from simsopt.util import MpiPartition
@@ -15,7 +18,7 @@ from simsopt import util
 import py_spec.output
 mpi = MpiPartition()
 only_plot = False
-freeboundary = True
+freeboundary = False
 if len(sys.argv)>=2:
     # User called the script with arguments, just plot the results
     filename = sys.argv[1]
@@ -28,14 +31,20 @@ if len(sys.argv)>=2:
         equil.run()
         surf = equil.boundary
         results = equil.results
+        only_plot = True
     elif filename.endswith(".h5"):
         results = py_spec.output.SPECout(filename)
         phiedge = results.input.physics.phiedge
         surf = mhd.Spec.pyspec_to_simsopt_surf(results, 0)
+        only_plot = True
+    elif filename == "--freeboundary":
+        freeboundary = True
+        print("FREEBOUNDARY")
     else:
         raise ValueError("Filename must end with .sp.end or .h5")
-    only_plot = True
-else:
+    
+
+if not only_plot:
     equil = mhd.Spec("hybrid_tokamak/laptop/rotating_ellipse_fb_low.sp", mpi, verbose=True, tolerance=1e-10, keep_all_files=True)
 
     equil.lib.inputlist.odetol = 1e-6
@@ -67,26 +76,31 @@ qs2 = mhd.Quasisymmetry(boozer,
                    0.5, # Radius to target
                    helicity_m=1, helicity_n=0) # (M, N) you want in |B|
 
+# Generate timestamp string for folder name 
+timestampdir = datetime.datetime.now().strftime(("freeb_" if freeboundary else "fixb_") +"%m-%d-%H-%M-%S")
+subprocess.check_output(["mkdir","-p", timestampdir])
+
 if not only_plot:
-    Bn = equil._normal_field  # This is our new fancy-pants degree of freedom :)
     surf.fix_all()
-    Bn.fix_all()
     R0 = 1 #surf.major_radius()
     # equil.unfix("phiedge")
 
-    for mmax in range(1, 3):
-        nmax = mmax
-        if freeboundary:
+    if freeboundary:
+        Bn = equil._normal_field  # This is our new fancy-pants degree of freedom :)
+        Bn.fix_all()
+
+        for mmax in range(1, 1):
+            nmax = mmax
             # set appropriate bounds for DOFs
             prev_dofs = np.array(Bn.local_dofs_free_status, dtype=bool).copy()
                 
             # Bn.fixed_range(0, mmax, -nmax, nmax, False) # unfix square region
             additional_dofs = np.logical_and(Bn.local_dofs_free_status, np.logical_not(prev_dofs))
             # higher fourier modes crash the simulation more easily
-            Bn.local_full_lower_bounds = np.where(additional_dofs, Bn.local_full_x - np.ones_like(Bn.local_full_x) * 3e-2/nmax, Bn.local_full_lower_bounds)
-            Bn.local_full_upper_bounds = np.where(additional_dofs, Bn.local_full_x + np.ones_like(Bn.local_full_x) * 3e-2/nmax, Bn.local_full_upper_bounds)
+            Bn.local_full_lower_bounds = np.where(additional_dofs, Bn.local_full_x - np.ones_like(Bn.local_full_x) * 2e-2/nmax, Bn.local_full_lower_bounds)
+            Bn.local_full_upper_bounds = np.where(additional_dofs, Bn.local_full_x + np.ones_like(Bn.local_full_x) * 2e-2/nmax, Bn.local_full_upper_bounds)
 
-    for mmax in range(3, 5):
+    for mmax in range(1, 4):
         nmax = mmax
         if freeboundary:
             # set appropriate bounds for DOFs
@@ -102,29 +116,55 @@ if not only_plot:
             logging.info(f"lower: {Bn.lower_bounds}")
         else:
             surf.fixed_range(0, mmax, -nmax, nmax, False)
-            # surf.fix("rc(0,0)")
+            surf.lower_bounds = -0.3 * np.ones_like(surf.lower_bounds)
+            surf.upper_bounds =  0.3 * np.ones_like(surf.upper_bounds)
+            surf.fix("rc(0,0)")
         
-        prob = objectives.LeastSquaresProblem.from_tuples(
-            [
-                # (equil.volume if freeboundary else surf.volume, initial_volume, 1),
-                # (surf.major_radius, R0, 1), # try to keep the major radius fixed
-                # (vmec.vacuum_well, -0.05, 1),
-                # (qs.residuals, 0, 1),
-
-                (vmec.iota_edge, 0.4384346834911653, 1),
-                (vmec.mean_iota, 0.412, 1), 
-                (surf.major_radius, R0, 3), # try to keep the major radius fixed
-                (vmec.vacuum_well, -0.01, 2),
-                (qs.residuals, 0, 1)
+        objs =[
+                (vmec.mean_iota, 0.4384346834911653, 1),  
+                (vmec.vacuum_well, -0.01, 1),
+                (qs.residuals, 0, 10),
+                # (qs2.residuals, 0, 10)
             ]
-        )
-        util.proc0_print(f"Free dofs of problem", prob.dof_names)
-        
-        least_squares_mpi_solve(prob, mpi, abs_step=1e-6, grad=True, 
-                                ftol=1e-06, xtol=5e-06, gtol=1e-06, max_nfev=40)
-        
-        subprocess.check_call(["cp", "hybrid_tokamak/laptop/rotating_ellipse_fb_low_00*", f"hybrid_tokamak/laptop/working_optimization/boundswradius{mmax}/"])
+        tulples_nlc = [
+            (vmec.mean_iota, 0.41, 0.47), 
+            (vmec.vacuum_well, -1, -0.005)
+        ]
+        if freeboundary:
+             # try to keep the major radius fixed
+            objs.append((surf.major_radius, R0, 3))
+        else:
+            # Since flux isn't constrained, we must fix the aspect ratio
+            objs.append((vmec.aspect, 30, 1))
+            tulples_nlc.append((vmec.aspect, 29, 31))
+            tulples_nlc.append((surf.major_radius, R0-1e-2, R0+1e-2))
 
+        prob = objectives.LeastSquaresProblem.from_tuples(objs)
+        # prob = objectives.ConstrainedProblem(qs.total, tulples_nlc)
+        util.proc0_print(f"Free dofs of problem", prob.dof_names)
+        kwargs = { }
+        if freeboundary:
+            kwargs["abs_step"] = 2e-6
+            kwargs["xtol"] = 5e-06
+            # Larger steps in the magnetic field modes are required to get clean gradients
+        if isinstance(prob, objectives.ConstrainedProblem):
+            constrained_mpi_solve(prob, mpi, grad=True, options={"maxiter":140}, **kwargs)
+        else:
+            if freeboundary:
+                kwargs["max_nfev"] = 30
+            
+            least_squares_mpi_solve(prob, mpi, grad=True, **kwargs)
+        
+        if mpi.proc0_world:
+            destpath = os.path.join(timestampdir, f"mpol{mmax}/")
+            if freeboundary:
+                srcpath ="hybrid_tokamak/laptop/rotating_ellipse_fb_low_000_*"
+            else:
+                srcpath = "input.rot_ellipse_*"
+            subprocess.check_call(["mkdir", "-p", destpath])
+            for filename in glob.glob(srcpath):
+                subprocess.check_call(["cp", filename, destpath])
+        
         util.proc0_print("At the resolution increase")
         util.proc0_print(" objective function =", prob.objective())
         util.proc0_print(" iota on axis       =", vmec.iota_axis())
