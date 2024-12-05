8b95539cd4c07faf42a1926301ce7c715cb59341
Philipp Jurasic <philipp.jurasic@tum.de>
2024-12-05 18:38:48 +0100

    lower resolution vmec input configuration, vmec plotting

--- hybrid_tokamak/laptop/plot_vmec.py
+++ hybrid_tokamak/laptop/plot_vmec.py
@@ -1,6 +1,20 @@
 import simsopt.mhd
 import sys
-
+import numpy as np
+import matplotlib.pyplot as plt
 if __name__ == "__main__": 
-    vmec = simsopt.mhd.Vmec(sys.argv[1])
-    vmec.boundary.plot()
+    vmecs = [simsopt.mhd.Vmec(filename) for filename in sys.argv[1:]]
+    # for phi in [0, np.pi/2, np.pi]:
+    for vmec in vmecs:
+        cross = vmec.boundary.cross_section(0)
+        plt.plot(cross[:,0], cross[:,2])
+    plt.legend(sys.argv[1:])
+    plt.show()
+    for filename in sys.argv[1:]:
+        vmec = simsopt.mhd.Vmec(filename)
+        # vmec.boundary.plot(show=False)
+        vmec.boundary.scale(4)
+        vmec.boundary.plot(show=False)
+    plt.show()
+
+    
