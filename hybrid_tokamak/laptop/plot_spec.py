ac4661c61ae63f32ef91b78c89a55639cbea5922
Philipp Jurasic <philipp.jurasic@tum.de>
2024-12-07 01:54:55 +0100

    plotting colors <3

--- hybrid_tokamak/laptop/plot_spec.py
+++ hybrid_tokamak/laptop/plot_spec.py
@@ -8,37 +8,36 @@ filenames = sys.argv[1:]
 fig, axs = plt.subplots(2, 3, figsize=(12, 4))
 axs = axs.flatten()
 
-colors = plt.cm.plasma(np.linspace(0, 1, len(filenames)))
-
-
-colors = ["r", "g", "b", "c", "m", "y", "k"]
-print(colors)
+colors = plt.cm.tab20b(np.linspace(0, 1, len(filenames)))
 colors = colors[:len(filenames)]
 for filename, c in zip(filenames, colors):
   try:
     out = py_spec.SPECout(filename) 
   except:
     print("Failed to read ", filename)
+    continue
   
   for i, ax in enumerate(axs):  
-    out.plot_kam_surface(ntheta=128, ax=ax, c=c, label=filename, zeta=i*np.pi/len(axs))
+    out.plot_kam_surface(ntheta=128, ax=ax, c=c[np.newaxis, :], label=filename, zeta=i*np.pi/len(axs))
     ax.set_xlabel("s")
     ax.set_ylabel("z")
     ax.set_title(f"Slice $\zeta = {i/len(axs):.2f} \pi$")
-    # if i == 0:
-    #   ax.legend()
+    if i == 0:
+      ax.legend(prop={'size': 6})
 # plt.legend(filenames)
 plt.show()
 
 for filename in filenames:
   try:
     out = py_spec.SPECout(filename) 
-    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
-    
-    out.plot_kam_surface(ax=axs[0])
-    out.plot_poincare(ax=axs[1])
-    out.plot_modB(np.linspace(-0.999, 1, 32), np.linspace(0, 2*np.pi, 32), ax=axs[2])
   except:
     print("Failed to read ", filename)
+    continue
+
+  fig, axs = plt.subplots(1, 3, sharex=True,  figsize=(12, 4))
+  
+  out.plot_kam_surface(ax=axs[0])
+  out.plot_poincare(ax=axs[1], s=1, )
+  out.plot_modB(np.linspace(-1+1e-6, 1, 64), np.linspace(0, 2*np.pi, 64), lvol=[0], ax=axs[2])
   
 plt.show()
